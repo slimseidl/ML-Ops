3a6d7416aefbb5ebbd3d374a8491f160452fd6c3
Slimseidl <john_seidl@outlook.com>
2025-06-06 08:47:48 -0500

    Ran tests via data_check steps

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,10 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data):
+    """Check dataset has certain nubmer of rows"""
+    assert 15000 < data.shape[0] < 1000000, f"Unexpected row count: {data.shape[0]}"
+
+def test_price_range(data, min_price, max_price):
+    """Ensure all price values fall within range"""
+    assert data['price'].between(min_price, max_price).all()
